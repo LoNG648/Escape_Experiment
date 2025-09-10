f34c3b58646f2f99ec0ea8d0dd68bdf9afc8c193
Ryan Pitney <rppitney@gmail.com>
2025-09-10 11:36:58 -0400

    Working more on Health System

---
+++ Components/HealthSystem.gd
@@ -0,0 +1,22 @@
+class_name Health
+extends Node
+
+@export var maxHealth: float = 100.0
+var currentHealth: float = maxHealth
+
+signal healthChanged(newHealth)
+
+func _ready() -> void:
+	print(maxHealth)
+	print(currentHealth)
+
+func takedamage(damage: float) -> void:
+	currentHealth -= damage
+	if currentHealth <= 0:
+		Dying()
+	emit_signal("healthChanged", currentHealth)
+func Dying():
+	get_tree().create_timer(1)
+	Died()
+func Died() -> void:
+	pass
