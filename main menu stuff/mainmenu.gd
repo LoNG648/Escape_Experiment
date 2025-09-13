216a7095bc1be454bb655cbb32214a0bcb1105f0
Nathan Pinnello <npinnello@gmail.com>
2025-09-13 17:26:29 -0400

    Added Ryan's stuff

--- main menu stuff/mainmenu.gd
+++ main menu stuff/mainmenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://test_level.tscn")
+	get_tree().change_scene_to_file("res://Levels/test_level.tscn")
 
 
 func _on_quit_pressed() -> void:
