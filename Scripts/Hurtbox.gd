1e4b9cec7433b981080de87af2d145e43efa24bf
Ryan Pitney <rppitney@gmail.com>
2025-09-16 18:12:08 -0400

    Changes

--- Scripts/Hurtbox.gd
+++ Scripts/Hurtbox.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var damage: float
 
-@onready var health_manager: Node = %HealthManager
+@onready var health_manager: HealthManager = %HealthManager
 
 func _on_body_entered(body: Node2D):
 	if body.health:
--- Scripts/healthmanager.gd
+++ Scripts/healthmanager.gd
@@ -1,3 +1,4 @@
+class_name HealthManager
 extends Node
 
 signal death()
@@ -5,7 +6,7 @@ signal death()
 func takeDamage(body: Node2D, damage: float) -> void:
 	if body.block != true:
 		body.get_node("Health").currentHealth -= damage
-		if body.name == "Player":
+		if body is player:
 			print("Ouch")
 			print(body.get_node("Health").currentHealth)
 		if body.get_node("Health").currentHealth <= 0:
