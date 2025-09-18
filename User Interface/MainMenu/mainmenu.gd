2a330a5bfaeac94da046e0cd3d5bda4023b1cad1
Nathan Pinnello <npinnello@gmail.com>
2025-09-17 20:32:26 -0400

    Added enemies dying

--- User Interface/MainMenu/mainmenu.gd
+++ User Interface/MainMenu/mainmenu.gd
@@ -10,3 +10,9 @@ func _on_quit_pressed() -> void:
 
 func _on_test_room_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/enemy_test_level.tscn")
+
+
+
+
+func _on_nathans_test_level_pressed() -> void:
+	get_tree().change_scene_to_file("res://Nathan's Stuff/levels/nathan_test_level.tscn")
