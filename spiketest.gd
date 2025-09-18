7908242566ebcdec6a0d4a6b7a4c0a5e6224dfda
Jack Morrow <146453969+Pentrel@users.noreply.github.com>
2025-09-17 20:45:12 -0400

    spike 2.0

--- spiketest.gd
+++ spiketest.gd
@@ -34,7 +34,7 @@ func _physics_process(delta: float) -> void:
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		
-		if collision.get_collider().name == "tilemapspikes":
+		if collision.get_collider().name == "tilemapspike":
 			if took_damage == false:
 				took_damage =true
 				respawn()
