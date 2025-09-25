05d6495c7c3e7d46c3fd0c81ef088fb61035e4e0
Ryan Pitney <rppitney@gmail.com>
2025-09-25 15:54:46 -0400

    Finalized Dev Mode Stuff for Main Branch

--- User Interface/MainMenu/mainmenu.gd
+++ User Interface/MainMenu/mainmenu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var DeveloperMode: bool
+var DeveloperName: String
 
 @onready var testing: Button = $VBoxContainer/Buttons/Testing
 @onready var dev_node: Node = $"Globals/Dev Node"
@@ -21,11 +22,16 @@ func _on_quit_pressed() -> void:
 func _on_test_room_pressed() -> void:
 	if dev_node.DeveloperName == "Gray":
 		get_tree().change_scene_to_file("res://Testing/Gray's Stuff/Gray Test Level.tscn")
+		Globals.set("DeveloperName", "Gray")
 	elif dev_node.DeveloperName == "Jack":
 		get_tree().change_scene_to_file("res://Testing/Jack's Stuff/Jack Test Level.tscn")
+		Globals.set("DeveloperName", "Jack")
 	elif dev_node.DeveloperName == "Max":
 		get_tree().change_scene_to_file("res://Testing/Max's Stuff/Max Test Level.tscn")
+		Globals.set("DeveloperName", "Max")
 	elif dev_node.DeveloperName == "Nathan":
 		get_tree().change_scene_to_file("res://Testing/Nathan's Stuff/Levels/Nathan Test Level.tscn")
+		Globals.set("DeveloperName", "Nathan")
 	elif dev_node.DeveloperName == "Ryan":
 		get_tree().change_scene_to_file("res://Testing/Ryan's Stuff/Ryan Test Level.tscn")
+		Globals.set("DeveloperName", "Ryan")
