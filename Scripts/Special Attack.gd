8f5f59e4bd54efe75acef9fd250784fc56296977
Ryan Pitney <rppitney@gmail.com>
2025-10-16 01:30:28 -0400

    Fixed and added animations for basic attacks and stuff

--- Scripts/Special Attack.gd
+++ Scripts/Special Attack.gd
@@ -9,12 +9,15 @@ extends Node2D
 
 #Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	dev_line.points.clear()
+	dev_line.add_point(special_attack_raycast.position)
+	dev_line.add_point(special_attack_raycast.target_position)
 	#Checks if Dev Mode is on and adds a line to help with testing the special attack
 	if Globals.DeveloperMode == true:
-		dev_line.points.clear()
+		#dev_line.points.clear()
 		dev_line.show()
-		dev_line.add_point(special_attack_raycast.position)
-		dev_line.add_point(special_attack_raycast.target_position)
+		#dev_line.add_point(special_attack_raycast.position)
+		#dev_line.add_point(special_attack_raycast.target_position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta) -> void:
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -19,10 +19,7 @@ var facing: bool = true #Which direction is the character facing (left is false,
 @onready var special_attack: Node2D = $"Special Attack" #Special Attack Variable
 @onready var basic_attack_hurtbox_collision: CollisionShape2D = $"Basic Attack Hurtbox/Basic Attack Hurtbox Collision" #Basic Attack Hurtbox
 @onready var counter_attack_hurtbox_collision: CollisionShape2D = $"Counter Attack Hurtbox/Counter Attack Hurtbox Collision" #Counter Attack Hurtbox
-@onready var basic_attack_timer: Timer = $"Timers/Basic Attack Timer"
 @onready var crouch_timer: Timer = $"Timers/Crouch Timer"
-@onready var stun_timer: Timer = $"Timers/Stun Timer"
-@onready var special_attack_timer: Timer = $"Timers/Special Attack Timer"
 @onready var counter_attack_timer: Timer = $"Timers/Counter Attack Timer"
 @onready var block_timer: Timer = $"Timers/Block Timer"
 @onready var animation_timer: Timer = $"Timers/Animation Timer"
@@ -33,6 +30,11 @@ func blockedDamage():
 	if Globals.DeveloperMode == true:
 		print("Nope!")
 	counterAttack = true
+	counter_attack_timer.start()
+	await counter_attack_timer.timeout
+	if Globals.DeveloperMode == true:
+		print("Counter Attack Lost")
+	counterAttack = false
 
 func _physics_process(delta: float) -> void:
 	#Get the input direction and handle the movement/deceleration
@@ -45,20 +47,7 @@ func _physics_process(delta: float) -> void:
 	#Disables code hereafter if dead to not allow player movement and attacks
 	if dead:
 		return
-		
-	#Handles jump
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	#Adjust Sprite and Hitbox
-	if Input.is_action_just_pressed("Move Left") and facing == true:
-			scale.x = abs(scale.x) * -1
-			facing = false
-	elif Input.is_action_just_pressed("Move Right") and facing == false:
-			scale.x = abs(scale.x) * -1
-			facing = true
-		
-	#Play Animations
+	
 	if animation_timer.get_time_left() == 0:
 		sprite.position = Vector2(0, -42)
 		if is_on_floor():
@@ -68,89 +57,114 @@ func _physics_process(delta: float) -> void:
 				sprite.play("Run")
 		else:
 			sprite.play("Jump")
-	
-	#Applies Horizontal Movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	move_and_slide()
-	
-	#Handles crouch and reduces Hitbox
-	if Input.is_action_just_pressed("Crouch"):
-		if crouch_timer.get_time_left() == 0:
-			scale.y /= 2
-		crouch_timer.paused = true
-	
-	#Sets a delay before crouching can be done again and resets hitbox
-	if Input.is_action_just_released("Crouch"):
-		if crouch_timer.get_time_left() != 0:
-			crouch_timer.paused = false
-			crouch_timer.start()
-			return
-		elif crouch_timer.get_time_left() == 0:
-			crouch_timer.paused = false
-			crouch_timer.start()
-		await crouch_timer.timeout
-		scale.y *= 2
-	
-	#Attack and Counterattack Mechanic
-	if Input.is_action_just_pressed("Basic Attack") and blocking == false and attacking == false:
+		#Handles jump
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
+		#Applies Horizontal Movement
+		if direction:
+			velocity.x = direction * SPEED
+			if velocity.x < 0 and facing == true:
+				scale.x = abs(scale.x) * -1
+				facing = false
+			if velocity.x > 0 and facing == false:
+				scale.x = abs(scale.x) * -1
+				facing = true
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		#Handles crouch and reduces Hitbox
+		if Input.is_action_just_pressed("Crouch"):
+			if crouch_timer.get_time_left() == 0:
+				scale.y /= 2
+			crouch_timer.paused = true
+		
+		#Sets a delay before crouching can be done again and resets hitbox
+		if Input.is_action_just_released("Crouch"):
+			if crouch_timer.get_time_left() != 0:
+				crouch_timer.paused = false
+				crouch_timer.start()
+				return
+			elif crouch_timer.get_time_left() == 0:
+				crouch_timer.paused = false
+				crouch_timer.start()
+			await crouch_timer.timeout
+			scale.y *= 2
+		
+		#Attack and Counterattack Mechanic
+		if Input.is_action_just_pressed("Basic Attack"):
 			print(counterAttack)
 			#Promotes the attack to a counter attack if possible
 			if counterAttack == true:
+				sprite.position = Vector2(14, -33)
 				sprite.play("Counter Attack")
 				attacking = true
+				animation_timer.start(2)
 				counter_attack_hurtbox_collision.disabled = false
 				await get_tree().create_timer(0.2).timeout
 				counter_attack_hurtbox_collision.disabled = true
-				await get_tree().create_timer(2.8).timeout #Counter Attack Delay
+				await animation_timer.timeout
 				attacking = false
 				counterAttack = false
 			else:
 				#Does a basic attack if can't do a counter attack
 				sprite.position = Vector2(14, -33)
 				sprite.play("Attack")
-				animation_timer.start()
+				animation_timer.start(1)
 				attacking = true
 				basic_attack_hurtbox_collision.disabled = false
 				await get_tree().create_timer(0.2).timeout
 				basic_attack_hurtbox_collision.disabled = true
-				await get_tree().create_timer(0.8).timeout #Basic Attack Delay
+				await animation_timer.timeout #Basic Attack Delay
 				attacking = false
-	
-	#Apply Blocking Mechanic
-	if Input.is_action_just_pressed("Block"):
-		#Disables blocking if attacking or blocking
-		if blocking or attacking == true:
-			return
-		else:
+		
+		#Apply Blocking Mechanic
+		if Input.is_action_just_pressed("Block"):
 			sprite.position = Vector2(14, -33)
-			animation_timer.start()
+			animation_timer.start(0.5)
 			sprite.play("Block")
 			blocking = true
-			await get_tree().create_timer(0.5).timeout #Creates a 0.3 second delay
+			block_timer.start()
+			await block_timer.timeout
 			sprite.pause()
 			animation_timer.paused = true
 		
-	#Handles changing character back to normal after block is completed
-	if Input.is_action_just_released("Block"):
-		sprite.play("Block")
-		animation_timer.paused = false
-		blocking = false
+		#Special Attack Mechanic which causes damage to enemy while bypassing the Hurtbox code
+		if Input.is_action_just_pressed("Special Attack"):
+			if Globals.DeveloperMode == true:
+				print("SPECIAL!")
+			sprite.position = Vector2(14, -33)
+			sprite.play("Special Attack")
+			special_attack.get_node("Dev Line").visible = true
+			animation_timer.start(0.25)
+			special_attack.get_node("Special Attack Raycast").enabled = true
+			if special_attack.get_node("Special Attack Raycast").is_colliding():
+				if special_attack.get_node("Special Attack Raycast").get_collider() is Hitbox:
+					special_attack.get_node("Special Attack Raycast").get_collider().get_parent().get_node("Health").takeDamage(special_attack.get_node("Special Attack Raycast").get_collider().get_parent(), 10)
+			await animation_timer.timeout
+			if Globals.DeveloperMode == false:
+				special_attack.get_node("Dev Line").visible = false
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED/60)
+		#Handles changing character back to normal after block is completed
+		if blocking == true:
+			if Input.is_action_just_pressed("Move Left") and facing == true:
+				scale.x = abs(scale.x) * -1
+				facing = false
+			elif Input.is_action_just_pressed("Move Right") and facing == false:
+				scale.x = abs(scale.x) * -1
+				facing = true
+				
+			if Input.is_action_just_released("Block"):
+				if block_timer.get_time_left() != 0:
+					await block_timer.timeout
+				sprite.play("Block")
+				animation_timer.paused = false
+				blocking = false
 	
-	#Special Attack Mechanic which causes damage to enemy while bypassing the Hurtbox code
-	if Input.is_action_just_pressed("Special Attack") and blocking == false and attacking == false:
-		if Globals.DeveloperMode == true:
-			print("SPECIAL!")
-		sprite.play("Special Attack")
-		special_attack.get_node("Special Attack Raycast").enabled = true
-		if special_attack.get_node("Special Attack Raycast").is_colliding():
-			if special_attack.get_node("Special Attack Raycast").get_collider() is Hitbox:
-				special_attack.get_node("Special Attack Raycast").get_collider().get_parent().get_node("Health").takeDamage(special_attack.get_node("Special Attack Raycast").get_collider().get_parent(), 10)
-		
-
+	move_and_slide()
+	
 func got_hit():
 	pass
 
@@ -159,6 +173,7 @@ func death():
 	#Only triggers if health is less than or equal to 0 and you aren't already dead
 	if health.currentHealth <= 0 and dead == false:
 		dead = true
+		sprite.play("Dying")
 		if Globals.DeveloperMode == true:
 			print("Im Dying")
 		for i in range(4):
