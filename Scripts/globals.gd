00896ba3663df2251d0418950d74064d7e329fb6
Max Stewart <145406936+llewxamstewart@users.noreply.github.com>
2025-11-13 11:12:05 -0500

    scripts for keycard (hopefully?)

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -2,9 +2,12 @@ extends Node
 
 var DeveloperMode: bool = false
 var DeveloperName: String = ""
+var KeyCollected: bool = false
 
 var player_position: Vector2 = Vector2.ZERO
 
 func update_player_position(new_position: Vector2):
 	player_position = new_position
 	
+
+var coin_count: int = 0
