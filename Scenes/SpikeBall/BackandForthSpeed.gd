998d90997949d2a706a67e5683f677cd3616fff4
Ryan Pitney <rppitney@gmail.com>
2025-11-13 14:24:48 -0500

    Revert "Merge branch 'Develop' into TestDevelop"
    This reverts commit 594609ecfe2297e212baf716636c1f1ef2e7feb7, reversing
    changes made to 170781a29dfb8034f47a8d7912e16827881baa56.

---
+++ Scenes/SpikeBall/BackandForthSpeed.gd
@@ -0,0 +1,10 @@
+extends PathFollow2D
+
+var direction = 1
+
+func _process(_delta):
+	progress_ratio += .008 * direction
+	if progress_ratio == 1:
+		direction = -1
+	if progress_ratio == 0:
+		direction = 1
