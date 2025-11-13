1600b64821ace9185495a7996534c8395e48abc9
Pentrel <146453969+Pentrel@users.noreply.github.com>
2025-11-12 23:06:04 -0500

    level stuff

--- Scenes/SpikeBall/BackandForthSpeed.gd
+++ Scenes/SpikeBall/BackandForthSpeed.gd
@@ -3,7 +3,7 @@ extends PathFollow2D
 var direction = 1
 
 func _process(_delta):
-	progress_ratio += .02 * direction
+	progress_ratio += .008 * direction
 	if progress_ratio == 1:
 		direction = -1
 	if progress_ratio == 0:
