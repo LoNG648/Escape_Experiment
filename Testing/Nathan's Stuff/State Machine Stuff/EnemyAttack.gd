15ae17af304c90fe92e96eb3e2955dc2d96282dc
Nathan Pinnello <npinnello@gmail.com>
2025-11-29 17:59:42 -0500

    state machine bugfixes

--- Testing/Nathan's Stuff/State Machine Stuff/EnemyAttack.gd
+++ Testing/Nathan's Stuff/State Machine Stuff/EnemyAttack.gd
@@ -7,30 +7,32 @@ class_name EnemyAttack
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var attacking = false
 
-
+@onready var animation_player = $"../../Sprite"
+@onready var hurtbox_collision = $"../../Hurtbox/Hurtbox Collision"
+#@onready var enemy = $"../.."
 
 func Enter(): 
-	$"../../Sprite".play("windup1")
-	#await
-	$"../../Sprite".play("attack1")
-	$"../../Hurtbox/Hurtbox Collision".disabled = false
+	animation_player.play("windup1")
+	await animation_player.animation_finished
+	animation_player.play("attack1")
+	hurtbox_collision.disabled = false
 	attacking = true
-	#await
-	$"../../Hurtbox/Hurtbox Collision".disabled = true
+	await animation_player.animation_finished
+	hurtbox_collision.disabled = true
 
 func Physics_Update(delta):
 	var player = get_tree().get_first_node_in_group("Player")
 	
-	enemy.velocity = attack_ms
+	enemy.velocity.x = 0
 	enemy.velocity.y = gravity * delta
 	
 	if attacking == true:
-		$"../../Sprite".play("windup1")
-		#await
-		$"../../Sprite".play("attack1")
-		$"../../Hurtbox/Hurtbox Collision".disabled = false
-		#await
-		$"../../Hurtbox/Hurtbox Collision".disabled = true
+		animation_player.play("windup1")
+		await animation_player.animation_finished
+		animation_player.play("attack1")
+		hurtbox_collision.disabled = false
+		await animation_player.animation_finished
+		hurtbox_collision.disabled = true
 		
 
 
@@ -38,3 +40,6 @@ func _on_front_detectionbox_body_exited(body: Node2D) -> void:
 	if body is Player:
 		attacking = false
 		Transitioned.emit(self, "EnemyFollow")
+
+func exit():
+	pass
--- Testing/Nathan's Stuff/basic_enemy_state.gd
+++ Testing/Nathan's Stuff/basic_enemy_state.gd
@@ -7,6 +7,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
+	
 	move_and_slide()
 	
 	if velocity.length() > 0:
