a0ef07159ca7b644be0ff729dac6a9771713eaeb
Nathan Pinnello <npinnello@gmail.com>
2025-12-07 12:42:48 -0500

    enemies done

--- Testing/Nathan's Stuff/State Machine Stuff/statemachine.gd
+++ Testing/Nathan's Stuff/State Machine Stuff/statemachine.gd
@@ -1,11 +1,16 @@
 extends Node
+class_name StateMachine  # Optional: add class_name for clarity
 
 @export var initial_state : State
+@export var hit_immunity_duration_min : float = 1.1
+@export var hit_immunity_duration_max : float = 1.3
 
 var current_state : State
 var states : Dictionary = {}
+var hit_immunity_timer : float = 0.0
 
 @onready var label = $"../Label"
+@onready var hitbox_collision : CollisionShape2D = $"../Hitbox/Hitbox Collision"  # Enemy's hurtbox (receives damage)
 
 func _ready():
 	for child in get_children():
@@ -15,15 +20,26 @@ func _ready():
 	if initial_state:
 		initial_state.Enter()
 		current_state = initial_state
+	hitbox_collision.disabled = false  # Ensure enabled initially
 
 func _process(delta):
 	if current_state:
 		current_state.Update(delta)
-	label.set_text(str(current_state))
+	label.set_text(str(current_state.name))  # .name for cleaner label
 
 func _physics_process(delta):
+	# Manage hitbox immunity globally
+	if hit_immunity_timer > 0:
+		hit_immunity_timer -= delta
+		hitbox_collision.disabled = true
+	else:
+		hitbox_collision.disabled = false
+	
 	if current_state:
 		current_state.Physics_Update(delta)
+
+func set_hit_immunity(duration: float):
+	hit_immunity_timer = duration
 
 func _on_child_transition(state, new_state_name):
 	if state != current_state:
